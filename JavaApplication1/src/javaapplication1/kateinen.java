1700aa392e02bcee53c2d72f01273446cafee909
berenton <56107579+berenton@users.noreply.github.com>
2020-05-20 11:08:12 +0300

    Lisätty tehtävänanto Lompakkoon.

---
+++ JavaApplication1/src/javaapplication1/kateinen.java
@@ -0,0 +1,7 @@
+/*
+Tänne samantyylinen luokka kuin Maksukortti,
+mutta saldon tulee olla viiden sentin tarkkuudella.
+Tee Lompakko-luokka, jolle voi luoda maksukortteja ja käteisolion.
+Kortilla maksaessa kuitit (ostoslista) tallentuvat maksukortille,
+käteisellä maksaessa kuitit tallentuvat Lompakkoon.
+ */
